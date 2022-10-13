2d717eb4cdfde1912d6fc07666f3f16240b2a703
Federico Navarro <navarrofederico14@gmail.com>
2022-10-13 20:44:23 -0300

    registrarLibro, modificar libro llamando a la base de datos

--- src/main/java/com/egg/biblioteca/controladores/LibroControlador.java
+++ src/main/java/com/egg/biblioteca/controladores/LibroControlador.java
@@ -7,6 +7,7 @@ package com.egg.biblioteca.controladores;
 
 import com.egg.biblioteca.entidades.Autor;
 import com.egg.biblioteca.entidades.Editorial;
+import com.egg.biblioteca.entidades.Libro;
 import com.egg.biblioteca.excepciones.MiException;
 import com.egg.biblioteca.services.AutorServicio;
 import com.egg.biblioteca.services.EditorialServicio;
@@ -43,33 +44,41 @@ public class LibroControlador {
     public String registrar(ModelMap modelo) {
         List<Autor> autores = autorServicio.listarAutores();
         List<Editorial> editoriales = editorialServicio.listarEditoriales();
-        
-        modelo.addAttribute("autores",autores);
-        modelo.addAttribute("editoriales",editoriales);
-        
+
+        modelo.addAttribute("autores", autores);
+        modelo.addAttribute("editoriales", editoriales);
+
         return "libro_form.html";
     }
 
     @PostMapping("/registro")
     public String registro(@RequestParam(required = false) Long isbn, @RequestParam String titulo, //en el parametro requerido= false para que no sea algo requerido.. 
-            @RequestParam(required = false) Integer ejemplares, 
+            @RequestParam(required = false) Integer ejemplares,
             @RequestParam String idAutor, @RequestParam String idEditorial, ModelMap modelo) {//la clase model map nos va servir para insertar en este modelo toda info que vamos a inyectar en el html
-        
-        try{
+
+        try {
             libroServicio.crearLibro(isbn, titulo, ejemplares, idAutor, idEditorial);//si todo sale bien retornamos al index.html
-            
-            modelo.put("exito" ,"El libro due cargado corretamente");
-        }catch (MiException ex){
-             List<Autor> autores = autorServicio.listarAutores();
-        List<Editorial> editoriales = editorialServicio.listarEditoriales();
-        
-        modelo.addAttribute("autores",autores);
-        modelo.addAttribute("editoriales",editoriales);
-           //  Logger.getLogger(LibroControlador.class.getName()).log(Level.SEVERE, null, ex);//se usaba para enviar al output
-             modelo.put("error", ex.getMessage());
-            return "libro_form.html";
+
+            modelo.put("exito", "El libro due cargado corretamente");
+        } catch (MiException ex) {
+            List<Autor> autores = autorServicio.listarAutores();
+            List<Editorial> editoriales = editorialServicio.listarEditoriales();
+
+            modelo.addAttribute("autores", autores);
+            modelo.addAttribute("editoriales", editoriales);
+            //  Logger.getLogger(LibroControlador.class.getName()).log(Level.SEVERE, null, ex);//se usaba para enviar al output
+            modelo.put("error", ex.getMessage());
+            return "libro_form.html";//volvemos a cargar el formulario
         }
         return "index.html";
     }
 
+    @GetMapping("/lista")
+    public String listar(ModelMap modelo) {
+
+        List<Libro> libros = libroServicio.listarLibros();
+        modelo.addAttribute("libros", libros);
+
+        return "libro_lista.html";
+    }
 }
